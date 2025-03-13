63fc35948471e63c2fdb58eb00d87d7573726cce
robfrank <413587+robfrank@users.noreply.github.com>
2025-03-13 16:20:05 +0000

    [create-pull-request] automated change

--- src/main/java/it/robfrank/linklift/application/port/in/NewLinkUseCase.java
+++ src/main/java/it/robfrank/linklift/application/port/in/NewLinkUseCase.java
@@ -1,7 +1,5 @@
 package it.robfrank.linklift.application.port.in;
 
-import it.robfrank.linklift.application.domain.model.Link;
-
 public interface NewLinkUseCase {
   boolean newLink(NewLinkCommand newLinkCommand);
 }

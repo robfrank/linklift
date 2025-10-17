e1d5e0b8bebf2a856c6513e0a4f9801fcb4d47c7
Roberto Franchini <ro.franchini@gmail.com>
2025-10-17 15:57:49 +0200

    feature: add content downloader and persistence ports

---
+++ src/main/java/it/robfrank/linklift/application/port/out/ContentDownloaderPort.java
@@ -0,0 +1,17 @@
+package it.robfrank.linklift.application.port.out;
+
+import java.util.concurrent.CompletableFuture;
+import org.jspecify.annotations.NonNull;
+
+public interface ContentDownloaderPort {
+
+    @NonNull
+    CompletableFuture<DownloadedContent> downloadContent(@NonNull String url);
+
+    record DownloadedContent(
+        @NonNull String htmlContent,
+        @NonNull String textContent,
+        @NonNull String mimeType,
+        int contentLength
+    ) {}
+}

67a186d660b282642170b5399ac47f0239e8c7b7
Roberto Franchini <ro.franchini@gmail.com>
2025-11-03 22:00:59 +0100

    chore: update formatting in .prettierrc.js and adjust indentation in deploy.yml

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 const config = {
-    printWidth: 160,
-    tabWidth: 4,
-    useTabs: false,
-    trailingComma: "none",
-    requirePragma: false,
-    plugins: [require.resolve("prettier-plugin-java"), require.resolve("@prettier/plugin-xml")]
+  printWidth: 160,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "none",
+  requirePragma: false,
+  plugins: [require.resolve("prettier-plugin-java"), require.resolve("@prettier/plugin-xml")]
 };
 
 module.exports = config;
